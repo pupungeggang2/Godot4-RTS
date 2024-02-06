a69188d917ac01e6084cc1bf99d6de0ac5957218
pupungeggang2 <kyongkyong2892@gmail.com>
2024-02-06 15:46:19 +0900

    0.0.0

--- Game/Script/Node/player.gd
+++ Game/Script/Node/player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var speed = 200
+@export var speed = 300.0
 var hp = 60
 var hp_max = 60
 var energy = 8
@@ -9,7 +9,25 @@ var energy_max = 8
 var destination = Vector2(0, 0)
 
 func _ready():
-	pass
+	position = Vector2(40, 40)
+	destination = Vector2(40, 40)
 	
 func _process(delta):
-	pass
+	if Var.state == '':
+		if Input.is_action_just_pressed("mouse_right"):
+			var mouse = get_viewport().get_mouse_position()
+			var x = mouse[0]
+			var y = mouse[1]
+			mouse_handle_right(x, y)
+		
+		move_player(delta)
+	
+func move_player(delta):
+	var diff = destination - position
+	
+	if diff.length() > 5:
+		var diff_normalized = diff.normalized()
+		position += diff_normalized * speed * delta
+		
+func mouse_handle_right(x, y):
+	destination = Vector2(x, y)
--- Game/Script/Scene/game.gd
+++ Game/Script/Scene/game.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
+@onready var node_player = get_node("Player")
+@onready var text_hp = get_node("UI/TextHP")
+@onready var text_energy = get_node("UI/TextEnergy")
+
 func _ready():
 	pass
 
 func _process(delta):
-	pass
+	update_ui()
+
+func update_ui():
+	text_hp.text = str(node_player.hp) + "/" + str(node_player.hp_max)
+	text_energy.text = str(node_player.energy) + "/" + str(node_player.energy_max)
